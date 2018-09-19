96ada091343adb8c73400ea6663acf9ade820a9e
Sebastien <sebastienlar619@gmail.com>
2018-09-19 11:04:32 +0400

    Initial Commit

---
+++ Miaou AdventuresTests/Miaou_AdventuresTests.swift
@@ -0,0 +1,34 @@
+//
+//  Miaou_AdventuresTests.swift
+//  Miaou AdventuresTests
+//
+//  Created by Sebastien Larue on 19/09/2018.
+//  Copyright © 2018 KangaGames. All rights reserved.
+//
+
+import XCTest
+@testable import Miaou_Adventures
+
+class Miaou_AdventuresTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
